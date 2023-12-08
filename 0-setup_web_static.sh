33aa11cd43ac12fbe2e9303afca4c294818e59e4
Mohammed Mubarak <modymu9@gmail.com>
2023-12-08 09:44:34 +0200

    change the ownership of /data/ after creating html file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,9 +17,9 @@ else
 	sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 fi
 
-sudo chown -hR ubuntu:ubuntu /data/
-
 echo "<h1 style=\"color: green;\">Test deploy script</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo chown -hR ubuntu:ubuntu /data/
 
 new_line="location /hbnb_static {"
 new_string="\t$new_line\n\t\talias /data/web_static/current/;\n\t}"
