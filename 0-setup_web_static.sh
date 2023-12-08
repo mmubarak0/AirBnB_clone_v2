395739267f58505fa8c18783407e15ed9d54a373
Mohammed Mubarak <modymu9@gmail.com>
2023-12-08 03:10:38 +0200

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+sudo apt -y update;
+sudo apt install -y nginx;
+
+sleep 5;
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -hR ubuntu:ubuntu /data/
+
+echo "<h1 style=\"color: green;\">Test deploy script</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+new_line="location /hbnb_static {"
+new_string="\t$new_line\n\t\talias /data/web_static/current/;\n\t}"
+file="/etc/nginx/sites-enabled/default"
+if ! grep -Pxq "\t$new_line" $file
+then
+        sudo sed -i "54i\\$new_string" $file;
+fi
+sudo service nginx restart
