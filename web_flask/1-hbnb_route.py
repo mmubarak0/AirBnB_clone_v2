fcdd4181cf05e7b2321d7fe27deab412ef9f293a
Mohammed Mubarak <modymu9@gmail.com>
2023-12-28 20:15:40 +0200

    HBNB C is fun! Python is cool!

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Flask application."""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    """Root path."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Hbnb path."""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Flask application."""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    """Root path."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Hbnb path."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """C text path."""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pytext(text="is cool"):
+    """Python text path."""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
