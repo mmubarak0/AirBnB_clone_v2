549249d7a6a6604651525f8456d6ea5c13dbe5ac
Mohammed Mubarak <modymu9@gmail.com>
2023-12-29 16:09:11 +0200

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Flask application."""
+from flask import Flask, abort, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """List all states."""
+    states = storage.all(State).values()
+    return render_template(
+        '7-states_list.html',
+        states=sorted(states, key=lambda e: e.name)
+    )
+
+
+@app.teardown_appcontext
+def downfunction(e):
+    """Teardown appcontext."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
